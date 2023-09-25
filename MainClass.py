2f9cf60ff0d204dcd7df4e2b8398876571003c83
RAGEJAX <109853879+ragej4x@users.noreply.github.com>
2023-09-25 19:24:08 +0000

    Add files via upload

--- MainClass.py
+++ MainClass.py
@@ -6,6 +6,11 @@ class MainClass():
 		self.x = x
 		self.y = y
 
+		self.left = False
+		self.right = False
+		self.down = False
+		self.up = False
+
 		self.speed = 2
 
 		self.red = (255,0,0)
@@ -31,15 +36,53 @@ class MainClass():
 
 		if keyinput[pg.K_w]:
 			self.yVel -= self.speed
+			self.up = True
+			self.down = False
+			self.left = False
+			self.right = False
 		
 		if keyinput[pg.K_s]:
 			self.yVel += self.speed
+			self.down = True
+			self.up = False
+			self.left = False
+			self.right = False
 
 		if keyinput[pg.K_d]:
 			self.xVel += self.speed
+			self.right = True
+			self.left = False
+			self.up = False
+			self.down = False
 
 		if keyinput[pg.K_a]:
 			self.xVel -= self.speed
+			self.left = True
+			self.right = False
+			self.up = False
+			self.down = False
+
+		#FIX
+
+		if keyinput[pg.K_a] and keyinput[pg.K_d]:
+			self.right = True
+			self.xVel += self.speed
+
+		elif keyinput[pg.K_w] and keyinput[pg.K_d]:
+			self.speed = 1.5
+
+		elif keyinput[pg.K_a] and keyinput[pg.K_s]:
+			self.speed = 1.5
+
+		elif keyinput[pg.K_w] and keyinput[pg.K_a]:
+			self.speed = 1.5
+
+		elif keyinput[pg.K_s] and keyinput[pg.K_d]:
+			self.speed = 1.5
+
+		else:
+			self.speed = 2
+
 
 
 		print(self.yVel, self.xVel)
@@ -55,6 +98,36 @@ class MainClass():
 		if not center.colliderect(self.lomiRect):
 			self.cameraX += self.cdx * self.cameraSpeed
 			self.cameraY += self.cdy * self.cameraSpeed
+
+
+
+	def updateAnimation(self, display, keyinput, pg):
+
+
+
+		if keyinput[pg.K_w] and self.up == True:
+			anim.runUpAnim(display, pg)
+		elif not keyinput[pg.K_w] and self.up == True:
+			anim.idleUpAnim(display, pg)
+
+		if keyinput[pg.K_s] and self.down == True:
+			anim.runDownAnim(display, pg)
+		
+		elif not keyinput[pg.K_s] and self.down == True:
+			anim.idleDownAnim(display, pg)
+
+		if keyinput[pg.K_d] and self.right == True:
+			anim.runRightAnim(display, pg)
+
+		elif not keyinput[pg.K_d] and self.right == True:
+			anim.idleRightAnim(display, pg)
+
+		if keyinput[pg.K_a] and self.left == True:
+			anim.runLeftAnim(display, pg)
+
+		elif not keyinput[pg.K_a] and self.left == True:
+			anim.idleLeftAnim(display, pg)
+
 
 
 class MapClass():
@@ -90,12 +163,105 @@ class MapClass():
 				y += 1
 
 class AnimationClass():
-	def __init__(self):
-		pass
+	def __init__(self, x ,y):
+		self.x = x
+		self.y = y
+
+		self.frameSpeed = 0.1
+
+		self.idleDownFrame = 1
+		self.idleUpFrame = 1
+		self.idleLeftFrame = 1
+		self.idleRightFrame = 1
+
+		self.runDownFrame = 1
+		self.runUpFrame = 1
+		self.runLeftFrame = 1
+		self.runRightFrame = 1
 
 
+
+	def idleDownAnim(self, display, pg):
+		idleImage = pg.image.load(f"data/assets/anim/totoy/idD{int(self.idleDownFrame)}.anim")
+		idleImage.set_colorkey((255,0,255))
+		display.blit(idleImage, (lomi.x - lomi.cameraX , lomi.y - lomi.cameraY - 15))
+
+		self.idleDownFrame += self.frameSpeed
+		if self.idleDownFrame > 6:
+			self.idleDownFrame = 1
+	
+	def idleUpAnim(self, display, pg):
+		idleImage = pg.image.load(f"data/assets/anim/totoy/idU{int(self.idleUpFrame)}.anim")
+		idleImage.set_colorkey((255,0,255))
+		display.blit(idleImage, (lomi.x - lomi.cameraX , lomi.y - lomi.cameraY - 15))
+
+		self.idleUpFrame += self.frameSpeed
+		if self.idleUpFrame > 6:
+			self.idleUpFrame = 1
+
+	def idleLeftAnim(self, display, pg):
+		idleImage = pg.image.load(f"data/assets/anim/totoy/id{int(self.idleLeftFrame)}.anim")
+		leftImage = pg.transform.flip(idleImage, True, False)
+		leftImage.set_colorkey((255,0,255))
+		display.blit(leftImage, (lomi.x - lomi.cameraX , lomi.y - lomi.cameraY - 15))
+
+		self.idleLeftFrame += self.frameSpeed
+		if self.idleLeftFrame > 6:
+			self.idleLeftFrame = 1
+
+	def idleRightAnim(self, display, pg):
+
+		idleImage = pg.image.load(f"data/assets/anim/totoy/id{int(self.idleRightFrame)}.anim")
+		idleImage.set_colorkey((255,0,255))
+		display.blit(idleImage, (lomi.x - lomi.cameraX , lomi.y - lomi.cameraY - 15))
+
+		self.idleRightFrame += self.frameSpeed
+		if self.idleRightFrame > 6:
+			self.idleRightFrame = 1
+
+	#RUN
+
+	def runDownAnim(self, display, pg):
+		runImage = pg.image.load(f"data/assets/anim/totoy/rD{int(self.runDownFrame)}.anim")
+		runImage.set_colorkey((255,0,255))
+		display.blit(runImage, (lomi.x - lomi.cameraX , lomi.y - lomi.cameraY - 15))
+
+		self.runDownFrame += self.frameSpeed
+		if self.runDownFrame > 6:
+			self.runDownFrame = 1
+	
+	def runUpAnim(self, display, pg):
+		runImage = pg.image.load(f"data/assets/anim/totoy/rU{int(self.runUpFrame)}.anim")
+		runImage.set_colorkey((255,0,255))
+		display.blit(runImage, (lomi.x - lomi.cameraX , lomi.y - lomi.cameraY - 15))
+
+		self.runUpFrame += self.frameSpeed
+		if self.runUpFrame > 6:
+			self.runUpFrame = 1
+
+	def runLeftAnim(self, display, pg):
+		runImage = pg.image.load(f"data/assets/anim/totoy/r{int(self.runLeftFrame)}.anim")
+		runImage = pg.transform.flip(runImage, True, False)
+		runImage.set_colorkey((255,0,255))
+		display.blit(runImage, (lomi.x - lomi.cameraX , lomi.y - lomi.cameraY - 15))
+
+		self.runLeftFrame += self.frameSpeed
+		if self.runLeftFrame > 6:
+			self.runLeftFrame = 1
+
+	def runRightAnim(self, display, pg):
+
+		runImage = pg.image.load(f"data/assets/anim/totoy/r{int(self.runRightFrame)}.anim")
+		runImage.set_colorkey((255,0,255))
+		display.blit(runImage, (lomi.x - lomi.cameraX , lomi.y - lomi.cameraY - 15))
+
+		self.runRightFrame += self.frameSpeed
+		if self.runRightFrame > 6:
+			self.runRightFrame = 1
+
+	
 
 lomi = MainClass(0,0)
 Map = MapClass()
-anim = AnimationClass()
+anim = AnimationClass(lomi.x, lomi.y)
 
