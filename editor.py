42db275c70c6f7b4ef26f1d6a002bd9556b75cf7
RAGEJAX <109853879+ragej4x@users.noreply.github.com>
2023-10-11 00:53:20 +0000

    optimized map editor

--- editor.py
+++ editor.py
@@ -1,121 +1,72 @@
-from MainClass import lomi
 import csv
-class editor_class():
-	def __init__(self):
-		import pygame as pg
-		self.world_data = []
-		self.max_col = 53
-		self.rows = 23
-		self.height = 900
-		self.width = 620
-		self.tile_size = self.height // self.rows - 8
-		self.tile = 0
+import pygame as pg
 
-		self.button_columns = 9
-		self.button_rows = 7
-		self.button_width = 16
-		self.button_hight = 16
-		self.buttons = []
+class Editor:
+    def __init__(self):
+        self.world_data = [[0] * 53 for _ in range(23)]
+        self.max_col = 53
+        self.rows = 23
+        self.height = 900
+        self.width = 620
+        self.tile_size = self.height // self.rows - 8
+        self.tile = 0
 
-		#LOAD IMAGE
-		self.item_list = []
-		self.item_count = 64
-	
-		for i in range(1, self.item_count + 1):
-			self.small_items = pg.image.load(f"data/assets/items/item ({i}).png")
-			self.small_items = pg.transform.scale(self.small_items, (self.tile_size, self.tile_size))
-			self.item_list.append(self.small_items)
+        self.button_columns = 9
+        self.button_rows = 7
+        self.buttons = []
 
+        self.item_list = [pg.transform.scale(pg.image.load(f"data/assets/items/item ({i}).png"), (self.tile_size, self.tile_size)) for i in range(1, 65)]
 
-		self.small_items_surface = pg.Surface((16, 16))
+        for row in range(self.button_rows):
+            for col in range(self.button_columns):
+                x = col * self.tile_size + 20 * self.tile_size
+                y = row * self.tile_size
+                text = f"Button {row * self.button_columns + col + 1}"
+                self.buttons.append({"rect": pg.Rect(x, y, self.tile_size, self.tile_size), "text": text, "item": row * self.button_columns + col + 1, "posx": x, "posy": y})
 
-		for row in range(self.rows):
-			r = [0] * self.max_col
-			self.world_data.append(r)
+    def draw_tile(self, display, keyinput, mx, my, mouseinput):
+        if mouseinput[0]:
+            for button in self.buttons:
+                if button["rect"].collidepoint(mx, my):
+                    self.tile = button['item']
 
-		#print(self.world_data)
+        for button in self.buttons:
+            display.blit(self.item_list[button["item"]], (button["posx"], button["posy"]))
 
+        for y, row in enumerate(self.world_data):
+            for x, tile in enumerate(row):
+                if tile >= 1:
+                    display.blit(self.item_list[tile], (x * self.tile_size, y * self.tile_size))
 
-	def draw_tile(self, pg, display, keyinput, mx , my, mouseinput):
-		#def draw_button(vx, vy, width, height, text):
-			#button_rect = pg.draw.rect(display, (255,255,255), (vx, vy, width, height), 2)
+    def draw_grid(self, display):
+        for c in range(self.max_col + 1):
+            pg.draw.line(display, (255, 255, 255), (c * self.tile_size, 0), (c * self.tile_size, self.height))
 
+        for c in range(self.rows + 1):
+            pg.draw.line(display, (255, 255, 255), (0, c * self.tile_size), (self.width, c * self.tile_size))
 
-		#draw tilesets
-		#if keyinput[pg.K_RIGHT]:
-		if mouseinput[0]:
-			for button in self.buttons:
-				if button["rect"].collidepoint(mx , my):
-					self.tile = button['item']
+    def update(self, mx, my, mouseinput, keyinput, display):
+        x = int(mx) // self.tile_size
+        y = int(my) // self.tile_size
 
-		for button in self.buttons:
+        if mx < self.width and my < self.height:
+            if mouseinput[0]:
+                if self.world_data[y][x] != self.tile:
+                    self.world_data[y][x] = self.tile
+            if mouseinput[2]:
+                self.world_data[y][x] = 0
 
-			#print(button["item"])
-			display.blit(self.item_list[button["item"]], (button["posx"], button["posy"]))
+        if keyinput[pg.K_p]:
+            with open("data/assets/map_1.data", "w", newline="") as file:
+                writer = csv.writer(file, delimiter=",")
+                for row in self.world_data:
+                    writer.writerow(row)
 
+        if keyinput[pg.K_o]:
+            with open("data/assets/map_1.data", newline='') as data:
+                reader = csv.reader(data, delimiter=",")
+                for x, row in enumerate(reader):
+                    for y, tile in enumerate(row):
+                        self.world_data[x][y] = int(tile)
 
-		for row in range(self.button_rows):
-			for col in range(self.button_columns):
-				x = col * (self.tile_size) + 20 * self.tile_size
-				y = row * (self.tile_size)
-				text = f"Button {row * self.button_columns + col + 1}"
-
-				#draw_button(x , y , self.tile_size, self.tile_size, text)
-				self.buttons.append({"rect": pg.Rect(x, y, self.tile_size, self.tile_size), "text":text, "item":row * self.button_columns + col + 1, "posx":x, "posy":y} )
-
-		for y, row in enumerate(self.world_data):
-			for x, tile in enumerate(row):
-				if tile >= 1:
-					display.blit(self.item_list[tile], (x * self.tile_size, y * self.tile_size))
-					#display.blit(self.small_items_surface, (x * self.tile_size	,y * self.tile_size))
-
-			
-
-	def draw_grid(self, pg, display):
-		#vertical lines
-		for c in range(self.max_col + 1):
-			pg.draw.line(display, (255,255,255), (c * self.tile_size, 0 ), (c * self.tile_size , self.height ))
-		#horizontal lines
-		for c in range(self.rows + 1):
-			pg.draw.line(display, (255,255,255), (0, c * self.tile_size ), (self.width , c * self.tile_size ))
-
-
-	def update(self, pg, mx, my, mouseinput, keyinput, display):
-		#CHECK COORD	
-		x = int(mx )// self.tile_size 
-		y = int(my )// self.tile_size
-
-		if mx < self.width and my < self.height:
-			if mouseinput[0]:
-				if self.world_data[y][x] != self.tile:
-					self.world_data[y][x] = self.tile
-					#print(self.world_data[y][x])
-			if mouseinput[2]:
-				self.world_data[y][x] = 0
-
-
-		#SAVE DATA
-		#print(x,y)
-
-		if keyinput[pg.K_p]:
-			#print("SAVED")
-			#print(self.world_data)
-			with open("data/assets/map_1.data", "w", newline="") as file:
-				writer = csv.writer(file, delimiter = ",")
-				for row in self.world_data:
-					writer.writerow(row)
-
-
-		#LOAD DATA
-		if keyinput[pg.K_o]:
-			with open("data/assets/map_1.data", newline='') as data:
-				reader = csv.reader(data, delimiter = ",")
-				for x, row in enumerate(reader):
-					for y, tile in enumerate(row):
-						self.world_data[x][y] = int(tile)
-
-
-
-
-
-main = editor_class()
+main = Editor()
